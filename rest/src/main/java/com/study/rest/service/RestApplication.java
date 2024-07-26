776f5f60d65d8efddc1e68a31393dc5668949214
mjy102097 <1020mjy@naver.com>
2024-07-26 14:05:47 +0900

    2024.07.26 springboot CRUD 끝

--- rest/src/main/java/com/study/rest/service/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package com.study.rest.service;
-
-import org.springframework.boot.SpringApplication;
-
-public class RestApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(RestApplication.class, args);
-    }
-}
---
+++ ssr/src/main/java/com/study/rest/dto/ReqGetListDto.java
@@ -0,0 +1,10 @@
+package com.study.rest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+public class ReqGetListDto {
+    private String company;
+    private String cpu;
+}
